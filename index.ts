486f766800f684b09b0c9b0cb5bedf4e50db20a0
Sameer Anis <159227804+Sameer-504@users.noreply.github.com>
2024-07-03 22:17:52 +0500

    Updated

--- index.ts
+++ index.ts
@@ -18,7 +18,6 @@ constructor(id:string,name:string,courseEnrolled:string[],feesAmount:number){
 let baseID:number  =  1000;
 let studentID:string = "";
 let continueEnrollment:boolean = true;
-
 let students: student[] = [];
 
 
@@ -40,15 +39,93 @@ if(action.ans === "Enroll a Student"){
             message:"Please enter Your Name:\n"
         })
 
-        let trimmedStudentName = (studentName.ans).trim().tolowerCase()
+        let trimmedStudentName = (studentName.ans).trim().toLowerCase()
         let studentNameCheck = students.map(obj => obj.name)
-
+        if(studentNameCheck.includes(trimmedStudentName) === false){
         if(trimmedStudentName !==""){
           baseID++
           studentID = "STID"+baseID
 
           console.log("\n\tYour Account has been Created");
           console.log(`Wellcome, ${trimmedStudentName}!` );
+
+          let course = await inquirer.prompt([{
+            type: "list",
+            name: "ans",
+            message: "Please select a course to enroll:",
+            choices: ["Java", "Python", "TypeScript", "C++", "C#"]
+          }])
+
+          let courseFees = 0;
+          switch(course.ans){
+            case "Java":
+              courseFees = 12000;
+              break;
+            case "Python":
+              courseFees = 10000;
+              break;
+            case "TypeScript":
+              courseFees = 8000;
+              break;
+            case "C++":
+              courseFees = 9000;
+              break;
+            case "C#":
+              courseFees = 15000;
+              break;
+          }
+
+          let courseConfirm =await inquirer.prompt({
+            type: "confirm",
+            name:"ans",
+            message: "Do  you want to enroll  in this Course"
+          })
+
+          if(courseConfirm.ans === true){
+            let Student = new student (studentID, trimmedStudentName, [course.ans], courseFees)
+
+            students.push(Student)
+
+            console.log("You Have Enrolled in this Course");
+          }
+
         }
+      }
   }
+
+  else if(action.ans === "Show Student status"){
+    if(students.length ! == 0){
+      let studentNameCheck = students.map(e => e.name)
+
+      let selectedStudent =await inquirer.prompt({
+        type: "list",
+        name: "ans",
+        message: "Please Select Name",
+        choices: studentNameCheck
+      })
+
+     
+      let foundStudent = students.find(Student => Student.name === selectedStudent.ans)
+      console.log("Student Information");
+      console.log(foundStudent);
+      console.log("\n");
+
+
+
+    }else{
+      console.log("Record is Empty");
+    }
+  }
+
+    let userConfirm =await inquirer.prompt({
+      type: "confirm",
+      name:"ans",
+      message: "Do you want to continue?"
+    })
+
+    if(userConfirm.ans === false){
+      continueEnrollment = false
+    }
+
+    
 } while(true)
