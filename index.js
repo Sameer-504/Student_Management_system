469052d618cefe462968d2595d5f761ecc459d32
Sameer Anis <159227804+Sameer-504@users.noreply.github.com>
2024-07-04 21:26:29 +0500

    updated

--- index.js
+++ index.js
@@ -2,28 +2,26 @@ import inquirer from "inquirer";
 class student {
     id;
     name;
-    courseEnrolled;
+    coursesEnrolled;
     feesAmount;
-    constructor(id, name, courseEnrolled, feesAmount) {
+    constructor(id, name, coursesEnrolled, feesAmount) {
         this.id = id;
         this.name = name;
-        this.courseEnrolled = courseEnrolled;
+        this.coursesEnrolled = coursesEnrolled;
         this.feesAmount = feesAmount;
     }
 }
-let baseID = 1000;
-let studentID = "";
+let baseId = 10000;
+let studentId = "";
 let continueEnrollment = true;
 let students = [];
 do {
-    let action = await inquirer.prompt([
-        {
-            name: "ans",
-            type: "list",
-            message: "Please select an Option:\n",
-            choices: ["Enroll a Student", "Show Student status"]
-        }
-    ]);
+    let action = await inquirer.prompt({
+        name: "ans",
+        type: "list",
+        message: "Please select an Option:\n",
+        choices: ["Enroll a Student", "Show Student status"]
+    });
     if (action.ans === "Enroll a Student") {
         let studentName = await inquirer.prompt({
             name: "ans",
@@ -34,55 +32,61 @@ do {
         let studentNameCheck = students.map(obj => obj.name);
         if (studentNameCheck.includes(trimmedStudentName) === false) {
             if (trimmedStudentName !== "") {
-                baseID++;
-                studentID = "STID" + baseID;
+                baseId++;
+                studentId = "STID" + baseId;
                 console.log("\n\tYour Account has been Created");
                 console.log(`Wellcome, ${trimmedStudentName}!`);
-                let course = await inquirer.prompt([{
-                        type: "list",
-                        name: "ans",
-                        message: "Please select a course to enroll:",
-                        choices: ["Java", "Python", "TypeScript", "C++", "C#"]
-                    }]);
-                let courseFees = 0;
-                switch (course.ans) {
+                let cource = await inquirer.prompt({
+                    type: "list",
+                    name: "ans",
+                    message: "Please select a course to enroll:",
+                    choices: ["Java", "Python", "TypeScript", "C++", "C#"]
+                });
+                let courceFees = 0;
+                switch (cource.ans) {
                     case "Java":
-                        courseFees = 12000;
+                        courceFees = 12000;
                         break;
                     case "Python":
-                        courseFees = 10000;
+                        courceFees = 10000;
                         break;
                     case "TypeScript":
-                        courseFees = 8000;
+                        courceFees = 8000;
                         break;
                     case "C++":
-                        courseFees = 9000;
+                        courceFees = 9000;
                         break;
                     case "C#":
-                        courseFees = 15000;
+                        courceFees = 15000;
                         break;
                 }
-                let courseConfirm = await inquirer.prompt({
+                let courceConfirm = await inquirer.prompt({
                     type: "confirm",
                     name: "ans",
                     message: "Do  you want to enroll  in this Course"
                 });
-                if (courseConfirm.ans === true) {
-                    let Student = new student(studentID, trimmedStudentName, [course.ans], courseFees);
+                if (courceConfirm.ans === true) {
+                    let Student = new student(studentId, trimmedStudentName, [cource.ans], courceFees);
                     students.push(Student);
                     console.log("You Have Enrolled in this Course");
                 }
             }
+            else {
+                console.log("Please enter a valid name");
+            }
+        }
+        else {
+            console.log("Student already exists");
         }
     }
     else if (action.ans === "Show Student status") {
         if (students.length == 0) {
-            let studentNameCheck = students.map(e => e.name);
+            let studentNamesCheck = students.map(e => e.name);
             let selectedStudent = await inquirer.prompt({
                 type: "list",
                 name: "ans",
                 message: "Please Select Name",
-                choices: studentNameCheck
+                choices: studentNamesCheck
             });
             let foundStudent = students.find(Student => Student.name === selectedStudent.ans);
             console.log("Student Information");
@@ -101,4 +105,4 @@ do {
     if (userConfirm.ans === false) {
         continueEnrollment = false;
     }
-} while (true);
+} while (continueEnrollment);
